46e4eaf27fab6ffbce4913b33885797d5da73bc7
Stamps <alexander.stamps@jacks.sdstate.edu>
2019-11-29 22:32:27 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SE340.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
